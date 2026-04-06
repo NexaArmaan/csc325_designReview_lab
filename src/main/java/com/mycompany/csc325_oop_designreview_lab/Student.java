6065b9a5814ab1cc966ebd70b4eb315894830fb0
Armaan Arora <armaanarora880@gmail.com>
2026-04-05 22:21:49 -0400

    Completed ToDo 3,4

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -11,26 +11,41 @@ package com.mycompany.csc325_oop_designreview_lab;
 
 // ToDo 1: Make this class a child of Human
 public class Student extends Human{
+    //Required variable
     private String address;
+    private float gpa;
 
     // ToDo 2: Fix the resulting errors
+    //Constructor
     public Student(String name, short age) {
         super(name, age);
     }
 
+    //Address getter method
     @Override
     public String getAddress() {
         return address;
     }
 
+    //Address Setter method
     @Override
     public void setAddress(String address) {
         this.address = address;
     }
 
-
-
 	// ToDo 3: Add a field for GPA and create a setter and a getter
-	
-	// ToDo 4: Add comments to your code
+    // ToDo 4: Add comments to your code
+
+    //GPA getter method
+    public float getGpa() {
+        return gpa;
+    }
+
+    //GPA setter method
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+
+
 }
